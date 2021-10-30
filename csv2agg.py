cecd0535df0718dfb9b299f01e9eb9af1462df81
Irek Romaniuk <irekromaniuk@Ireks-MacBook-Air.local>
2021-10-30 12:25:07 -0400

    Updated

--- csv2agg.py
+++ csv2agg.py
@@ -4,8 +4,16 @@ import sys, os
 if (len(sys.argv)<2):
     sys.exit("CSV file name missing")
 df = pd.read_csv(sys.argv[1])
-#print(df)
-#print(df.filter(regex='host').min())
-print(df.filter(regex='host').rename(columns=lambda x: x.split("#")[1]).agg(['min','max','mean']))
-print(df.filter(regex='host').rename(columns=lambda x: x.split("#")[1]).min().min(),df.filter(regex='host').rename(columns=lambda x: x.split("#")[1]).max().max(),
-df.filter(regex='host').rename(columns=lambda x: x.split("#")[1]).mean().mean())
+f1=df.filter(regex='host').rename(columns=lambda x: x.split("#")[1]).agg(['min','max','mean']).transpose()
+#print(f1)
+f2 = pd.DataFrame(
+        {
+        "min": [df.filter(regex='host').rename(columns=lambda x: x.split("#")[1]).min().min()],
+        "max": [df.filter(regex='host').rename(columns=lambda x: x.split("#")[1]).max().max()],
+        "mean": [df.filter(regex='host').rename(columns=lambda x: x.split("#")[1]).mean().mean()],
+        },
+        index=["ALL"],
+   )
+#print(f2)   
+result = pd.concat([f1, f2])
+print(result)
