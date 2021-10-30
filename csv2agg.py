d25a10081fbceb3705f83b0777902f89089bf6b2
Irek Romaniuk <irekromaniuk@Ireks-MacBook-Air.local>
2021-10-30 08:36:26 -0400

    Updated

--- csv2agg.py
+++ csv2agg.py
@@ -4,11 +4,8 @@ import sys, os
 if (len(sys.argv)<2):
     sys.exit("CSV file name missing")
 df = pd.read_csv(sys.argv[1])
-print(df)
-#print(df['SourceIP'][1])
-sys.exit("Exited")
-print(f"File: {sys.argv[1]}\n{df.count()} lines")
-for index, row in df.iterrows():
-    #   if row['Disabled'] =='skip':
-    #         continue
-    pass
+#print(df)
+#print(df.filter(regex='host').min())
+print(df.filter(regex='host').rename(columns=lambda x: x.split("#")[1]).agg(['min','max','mean']))
+print(df.filter(regex='host').rename(columns=lambda x: x.split("#")[1]).min().min(),df.filter(regex='host').rename(columns=lambda x: x.split("#")[1]).max().max(),
+df.filter(regex='host').rename(columns=lambda x: x.split("#")[1]).mean().mean())
